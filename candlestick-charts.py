0ae8cd5c9f8a88bce2b94c2263cef4375ab951db
Kyle Khai Tran <kylekhai04@gmail.com>
2023-08-01 12:43:05 -0400

    Add finance and visual data dependencies

---
+++ candlestick-charts.py
@@ -0,0 +1,14 @@
+#Importing modules
+
+#Data visualizer
+import matplotlib.pyplot as plt 
+
+#Access financial data from the web/API
+import pandas_datareader as web
+
+#Library to visualize financial and candlestick charts
+import mplfinance as mpf
+
+#Work with dates and times 
+import datetime as dt
+
